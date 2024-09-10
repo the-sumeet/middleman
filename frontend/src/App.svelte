70a95cec0b4fc9e1ef61be17af1a4bcdf1f575fe
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-09-10 10:56:44 +0530

    added start server icon

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -45,20 +45,7 @@
       href="#"
       class="p-1.5 text-gray-500 focus:outline-nones transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800 hover:bg-gray-100"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-        />
-      </svg>
+      <i class="text-3xl bi bi-play"></i>
     </a>
 
     <a
