c0f5243e0c8211036be593157fb2aa4af96225cf
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-09-18 01:30:47 +0530

    role name constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -3,7 +3,11 @@
 export const REDIRECT_TYPE = "redirect";
 
 // Rules
-export const REDIRECT_RULE = "redirect";
+export const RULE_REDIRECT = "redirect";
+export const RULE_MOD_BODY = "redirect";
+export const RULE_MOD_HEADER = "redirect";
+export const RULE_CANCEL = "cancel";
+export const RULE_DELAY= "redirect";
 
 // Tabs
 export const HOME = "home";
