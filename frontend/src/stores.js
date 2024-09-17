4c69d9db7bdd1c92dc1395a9cd6be7889a7da2b9
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-09-18 01:30:34 +0530

    cancels store

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,15 +1,19 @@
 import { writable } from "svelte/store";
-import { REDIRECT_RULE, REQUESTS } from "./constants";
-import { GetRedirects } from "../wailsjs/go/main/App";
+import { RULE_REDIRECT } from "./constants";
+import { GetCancels, GetRedirects } from "../wailsjs/go/main/App";
 import { HOME } from "./constants";
 
 // This is object and not int because integer does not refresh store when assigned same value.
-export const currentCollection = writable({collectionId: -1});
-export const currentRule = writable(REDIRECT_RULE);
+export const currentRule = writable(RULE_REDIRECT);
 export const redirects = writable([]);
 GetRedirects().then((res) => {
-    console.log(res);
+    console.log("Redirects", res.redirects);
     redirects.set(res.redirects);
+});
+export const cancels = writable([]);
+GetCancels().then((res) => {
+    console.log("Cancels", res.cancels);
+    cancels.set(res.cancels);
 });
 
 export const serverRunning = writable(false);
