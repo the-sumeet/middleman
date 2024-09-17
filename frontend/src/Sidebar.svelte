d703a91ac124d5fd324e6a38e410f67dcd6d947d
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-09-18 01:30:23 +0530

    removed logo

--- frontend/src/Sidebar.svelte
+++ frontend/src/Sidebar.svelte
@@ -10,7 +10,7 @@
     let currPage;
     const activePageCss = "text-blue-500 bg-gray-800";
     const inactivePageCss = "text-gray-200 hover:bg-gray-800";
-    
+
     const unCurrentPage = currentPage.subscribe((value) => {
         currPage = value;
     });
@@ -27,13 +27,13 @@
 <div
     class="flex flex-col items-center w-16 h-screen py-8 space-y-8 bg-gray-900 b border-r border-gray-800"
 >
-    <a href="#!">
+    <!-- <a href="#!">
         <img
             class="w-auto h-6"
             src="https://merakiui.com/images/logo.svg"
             alt=""
         />
-    </a>
+    </a> -->
 
     <a
         on:click={() => setPage(HOME)}
@@ -53,7 +53,9 @@
         <button type="button" class="relative">
             <i class="text-2xl bi bi-hdd-network"></i>
             <div
-                class="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white {$serverRunning ? "bg-green-500" : "bg-red-500"} border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900"
+                class="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white {$serverRunning
+                    ? 'bg-green-500'
+                    : 'bg-red-500'} border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900"
             ></div>
         </button>
 
