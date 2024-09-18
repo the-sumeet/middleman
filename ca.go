652d7ede95173a3711c7c1238ec4f422c073c37f
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-09-18 22:21:01 +0530

    remove genCert

--- ca.go
+++ ca.go
@@ -1,12 +1,10 @@
 package main
 
-import "os/exec"
+// func genCert() {
 
-func genCert() {
+// 	// ToDo: generate cert in pure Go.
+// 	certPath, keyPath := getCertKeyPath()
 
-	// ToDo: generate cert in pure Go.
-	certPath, keyPath := getCertKeyPath()
-
-	exec.Command("openssl", "genrsa", "-out", keyPath, "2048").Run()
-	exec.Command("openssl", "pkcs12", "-export ", "-new", "rsa:4096", "-key", keyPath, "-out", certPath, "-days", "3650", "-subj", "/O=Middleman").Run()
-}
+// 	exec.Command("openssl", "genrsa", "-out", keyPath, "2048").Run()
+// 	exec.Command("openssl", "pkcs12", "-export ", "-new", "rsa:4096", "-key", keyPath, "-out", certPath, "-days", "3650", "-subj", "/O=Middleman").Run()
+// }
