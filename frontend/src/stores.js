7c257035ac3804365bea0103bef879b003ce8e88
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-09-20 22:52:23 +0530

    added delays store

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,21 +1,22 @@
 import { writable } from "svelte/store";
 import { RULE_REDIRECT } from "./constants";
-import { GetCancels, GetRedirects } from "../wailsjs/go/main/App";
+import { GetCancels, GetDelays, GetRedirects } from "../wailsjs/go/main/App";
 import { HOME } from "./constants";
 
 // This is object and not int because integer does not refresh store when assigned same value.
 export const currentRule = writable(RULE_REDIRECT);
 export const redirects = writable([]);
 GetRedirects().then((res) => {
-    console.log("Redirects", res.redirects);
     redirects.set(res.redirects);
 });
 export const cancels = writable([]);
 GetCancels().then((res) => {
-    console.log("Cancels", res.cancels);
     cancels.set(res.cancels);
 });
-
+export const delays = writable([]);
+GetDelays().then((res) => {
+    delays.set(res.delays);
+});
 export const serverRunning = writable(false);
 // Leftmost sidebar buttons
 export const currentPage = writable(HOME);
