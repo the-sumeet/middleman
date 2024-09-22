0a561a323e9eea20908596e43969bbdfe5715e95
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-09-22 07:33:03 +0530

    autogen

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,23 +1,5 @@
 export namespace main {
 	
-	export class Cancel {
-	    entity: string;
-	    op: string;
-	    value: string;
-	    toType: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new Cancel(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.entity = source["entity"];
-	        this.op = source["op"];
-	        this.value = source["value"];
-	        this.toType = source["toType"];
-	    }
-	}
 	export class Config {
 	    serverPort: string;
 	    certPath: string;
@@ -54,6 +36,24 @@ export namespace main {
 	        this.delaySec = source["delaySec"];
 	    }
 	}
+	export class Cancel {
+	    entity: string;
+	    op: string;
+	    value: string;
+	    toType: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Cancel(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.entity = source["entity"];
+	        this.op = source["op"];
+	        this.value = source["value"];
+	        this.toType = source["toType"];
+	    }
+	}
 	export class Redirect {
 	    entity: string;
 	    op: string;
@@ -76,6 +76,42 @@ export namespace main {
 	        this.enabled = source["enabled"];
 	    }
 	}
+	export class InValue {
+	    redirect: Redirect;
+	    // Go type: Cancel
+	    cancel: any;
+	    delay: Delay;
+	
+	    static createFrom(source: any = {}) {
+	        return new InValue(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.redirect = this.convertValues(source["redirect"], Redirect);
+	        this.cancel = this.convertValues(source["cancel"], null);
+	        this.delay = this.convertValues(source["delay"], Delay);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	
 	export class ReturnValue {
 	    redirects: Redirect[];
 	    cancels: Cancel[];
