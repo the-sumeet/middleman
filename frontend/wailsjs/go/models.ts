5a4ff07a0b16d8b5f1d929f7bbf8c59e256aa17a
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-09-29 16:43:47 +0530

    autogen

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -50,6 +50,60 @@ export namespace main {
 	        this.delaySec = source["delaySec"];
 	    }
 	}
+	export class Header {
+	    action: string;
+	    isRequest: boolean;
+	    name: string;
+	    value: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Header(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.action = source["action"];
+	        this.isRequest = source["isRequest"];
+	        this.name = source["name"];
+	        this.value = source["value"];
+	    }
+	}
+	export class ModifyHeader {
+	    entity: string;
+	    op: string;
+	    value: string;
+	    mods: Header[];
+	
+	    static createFrom(source: any = {}) {
+	        return new ModifyHeader(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.entity = source["entity"];
+	        this.op = source["op"];
+	        this.value = source["value"];
+	        this.mods = this.convertValues(source["mods"], Header);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
 	export class Redirect {
 	    entity: string;
 	    op: string;
@@ -76,6 +130,7 @@ export namespace main {
 	    redirect: Redirect;
 	    cancel: Cancel;
 	    delay: Delay;
+	    modifyHeader: ModifyHeader;
 	
 	    static createFrom(source: any = {}) {
 	        return new InValue(source);
@@ -86,6 +141,7 @@ export namespace main {
 	        this.redirect = this.convertValues(source["redirect"], Redirect);
 	        this.cancel = this.convertValues(source["cancel"], Cancel);
 	        this.delay = this.convertValues(source["delay"], Delay);
+	        this.modifyHeader = this.convertValues(source["modifyHeader"], ModifyHeader);
 	    }
 	
 		convertValues(a: any, classs: any, asMap: boolean = false): any {
@@ -107,10 +163,12 @@ export namespace main {
 		}
 	}
 	
+	
 	export class ReturnValue {
 	    redirects: Redirect[];
 	    cancels: Cancel[];
 	    delays: Delay[];
+	    modifyHeaders: ModifyHeader[];
 	    error: string;
 	
 	    static createFrom(source: any = {}) {
@@ -122,6 +180,7 @@ export namespace main {
 	        this.redirects = this.convertValues(source["redirects"], Redirect);
 	        this.cancels = this.convertValues(source["cancels"], Cancel);
 	        this.delays = this.convertValues(source["delays"], Delay);
+	        this.modifyHeaders = this.convertValues(source["modifyHeaders"], ModifyHeader);
 	        this.error = source["error"];
 	    }
 	
