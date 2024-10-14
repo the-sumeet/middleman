cff159d9484bcac970b48683bed034a163e776ea
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-10-14 16:39:25 +0530

    refactor selected role css

--- frontend/src/screens/rules/RuleTile.svelte
+++ frontend/src/screens/rules/RuleTile.svelte
@@ -5,7 +5,8 @@
 
     import { currentRule } from "../../stores";
 
-    const selectedCss = "bg-gray-100 dark:bg-gray-800";
+    const selectedCss = "bg-gray-100 dark:bg-gray-700";
+    const unselectedCss = "dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100";
 
     function select() {
         currentRule.set(rule);
@@ -16,7 +17,7 @@
     on:click={select}
     class="{$currentRule == rule
         ? selectedCss
-        : ''} flex space-x-2 items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none"
+        : unselectedCss} flex space-x-2 items-center w-full px-5 py-2 transition-colors duration-200 focus:outline-none"
 >
     <div class="flex gap-2 items-center text-left rtl:text-right">
         <i class="text-xl font-bold text-white {icon}"></i>
