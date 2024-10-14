8db2039449651f208181ed7987cb81ce08112ec6
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-10-14 13:56:19 +0530

    highlight the rule if selected

--- frontend/src/screens/rules/RuleTile.svelte
+++ frontend/src/screens/rules/RuleTile.svelte
@@ -1,10 +1,12 @@
 <script>
     export let title;
     export let rule;
-    export let icon="text-xl font-bold text-white bi bi-shuffle";
-    
+    export let icon = "text-xl font-bold text-white bi bi-shuffle";
+
     import { currentRule } from "../../stores";
-  
+
+    const selectedCss = "bg-gray-100 dark:bg-gray-800";
+
     function select() {
         currentRule.set(rule);
     }
@@ -12,15 +14,16 @@
 
 <button
     on:click={select}
-    class="flex space-x-2 items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none"
+    class="{$currentRule == rule
+        ? selectedCss
+        : ''} flex space-x-2 items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none"
 >
-
     <div class="flex gap-2 items-center text-left rtl:text-right">
         <i class="text-xl font-bold text-white {icon}"></i>
         <h1
             class="text-sm font-medium text-gray-700 capitalize dark:text-white"
         >
-         {title}
+            {title}
         </h1>
 
         <!-- <p class="text-xs text-gray-500 dark:text-gray-400">11.2 Followers</p> -->
