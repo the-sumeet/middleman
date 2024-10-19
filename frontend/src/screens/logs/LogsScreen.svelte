6e93303d30d23ef963ecec90af5235bac43e901d
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-10-20 02:10:27 +0530

    rmove container class

--- frontend/src/screens/logs/LogsScreen.svelte
+++ frontend/src/screens/logs/LogsScreen.svelte
@@ -10,7 +10,7 @@
   });
 </script>
 
-<section class="mt-4 h-screen container px-4 mx-auto">
+<div class="mt-4 h-screen p-4 mx-auto w-full">
   <div class="flex flex-col h-full">
     <h2 class="text-2xl font-medium text-gray-800 dark:text-white">Logs</h2>
 
@@ -34,26 +34,26 @@
                   <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
                     <div class="flex space-x-2 items-center">
                       {#if log.method && log.method != ""}
-                          <div
-                            class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800"
-                          >
-                            {log.method}
-                          </div>
-                        {/if}
-                        {#if log.host && log.host != ""}
-                          <div
-                            class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800"
-                          >
-                            {log.host}
-                          </div>
-                        {/if}
-                        {#if log.path && log.path != ""}
-                          <div
-                            class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800"
-                          >
-                            {log.path}
-                          </div>
-                        {/if}
+                        <div
+                          class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800"
+                        >
+                          {log.method}
+                        </div>
+                      {/if}
+                      {#if log.host && log.host != ""}
+                        <div
+                          class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800"
+                        >
+                          {log.host}
+                        </div>
+                      {/if}
+                      {#if log.path && log.path != ""}
+                        <div
+                          class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800"
+                        >
+                          {log.path}
+                        </div>
+                      {/if}
                     </div>
                   </td>
 
@@ -85,4 +85,4 @@
       </div>
     </div>
   </div>
-</section>
+</div>
