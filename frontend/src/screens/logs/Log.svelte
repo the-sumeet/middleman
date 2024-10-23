1a12b99b069007a727e9b0a58232dd581af0d3f8
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-10-24 03:16:01 +0530

    added toggle expand features, but not using yet

--- frontend/src/screens/logs/Log.svelte
+++ frontend/src/screens/logs/Log.svelte
@@ -1,18 +1,59 @@
 <script>
   export let log;
+  import { onMount } from "svelte";
+  import * as ace from "brace";
 
+  import "brace/mode/html";
+  import "brace/mode/json";
+  import "brace/mode/text";
+  import "brace/mode/xml";
+  import "brace/mode/yaml";
+  import "brace/theme/dracula";
   import RuleIcons from "./RuleIcons.svelte";
+  // Randon id
+
+  let expanded = false;
+  let requestEditor;
+  let responseEditor;
+  const random = crypto.randomUUID();
+  const requestEditorId = `requestBody${random}`;
+  const responseEditorId = `responseBody${random}`;
 
   console.log(log);
 
   function getUrl() {
     return `${log.scheme}://${log.host}${log.path}`;
   }
+
+  function toggleExpand() {
+    if (expanded) {
+      requestEditor.destroy();
+      responseEditor.destroy();
+      expanded = false;
+    } else {
+      requestEditor = ace.edit(requestEditorId);
+      requestEditor.setTheme("ace/theme/dracula");
+      requestEditor.setValue(log.requestBody, 1);
+      requestEditor.setReadOnly(true);
+      requestEditor.setFontSize("13px");
+      requestEditor.setShowPrintMargin(false);
+
+      responseEditor = ace.edit(responseEditorId);
+      responseEditor.setTheme("ace/theme/dracula");
+      responseEditor.setValue(log.responseBody, 1);
+      responseEditor.setReadOnly(true);
+      responseEditor.setFontSize("13px");
+      responseEditor.setShowPrintMargin(false);
+
+      expanded = true;
+    }
+  }
 </script>
 
 <div class="mt-4">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
   <div
-    class="flex flex-col bg-gray-800 p-2 rounded border-2 border-gray-700 items-start overflow-x-auto"
+    class="cursor-pointer flex flex-col bg-gray-800 hover:border-blue-600 p-2 rounded border-2 border-gray-700 items-start overflow-x-auto"
   >
     <!-- Datetime -->
     <p class="text-white text-xs">{new Date(log.timestamp)}</p>
@@ -21,19 +62,37 @@
       <RuleIcons {log} />
     {/if}
 
-    <div class="mt-2 flex space-x-4 justify-between">
-      <!-- Right Side -->
-      <div class="flex gap-2 items-center">
-        <!-- Text -->
-        <div class="flex gap-2 text-md text-emerald-500">
-          <div>{log.method}</div>
-          <div class="break-all">{getUrl()}</div>
+    <div class="mt-2 flex gap-2 items-center">
+      <!-- Text -->
+      <div class="flex gap-2 text-md text-emerald-500">
+        <div>{log.method}</div>
+        <div class="break-all">{getUrl()}</div>
+      </div>
+    </div>
+
+    <!-- Content on expant -->
+      <div class="{expanded == true ? 'flex' : 'hidden'} flex-col w-full">
+        <!-- Request and response body -->
+        <div class="flex gap-2">
+          <!-- Request Body -->
+          <div class="flex flex-col w-full mt-4">
+            <p class="text-white">Request Body</p>
+            <div
+              id={requestEditorId}
+              class="border border-gray-700 mt-2 w-full rounded"
+              style="height: 128px;"
+            ></div>
+          </div>
+          <!-- Response Body -->
+          <div class="flex flex-col w-full mt-4">
+            <p class="text-white">Response Body</p>
+            <div
+              id={responseEditorId}
+              class="border border-gray-700 mt-2 w-full rounded"
+              style="height: 128px;"
+            ></div>
+          </div>
         </div>
       </div>
-
-      <!-- Expand / Collapse -->
-      <!-- <i class="text-2xl text-white bi bi-chevron-down"></i> -->
-      <!-- <i class="text-2xl text-gray-800 dark:text-white bi bi-chevron-up"></i> -->
-    </div>
   </div>
 </div>
