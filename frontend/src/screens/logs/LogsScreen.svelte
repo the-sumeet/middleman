be3a9611f63776658c957be8553346b80ab9a34d
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-10-23 14:12:54 +0530

    fetch new logs each sec

--- frontend/src/screens/logs/LogsScreen.svelte
+++ frontend/src/screens/logs/LogsScreen.svelte
@@ -1,19 +1,32 @@
 <script>
   import { GetLogs } from "../../../wailsjs/go/main/App";
   import Log from "./Log.svelte";
+  import { scrollToBottomIfAtBottom } from "../../../src/utils";
+
   let logs = [];
 
-  GetLogs().then((res) => {
-    if (res.httpRequests) {
-      logs = res.httpRequests;
-    }
-  });
+  fetchLogs();
+
+  function fetchLogs() {
+    GetLogs().then((res) => {
+      if (res.httpRequests) {
+        logs = res.httpRequests;
+        scrollToBottomIfAtBottom(document.getElementById("logs"));
+      }
+    });
+  }
+
+  let clear;
+  $: {
+    clearInterval(clear);
+    clear = setInterval(fetchLogs, 1000);
+  }
 </script>
 
 <div class="flex flex-col h-screen p-4 w-full">
   <h2 class="text-2xl font-medium text-white">Logs</h2>
 
-  <div class="flex flex-col overflow-y-auto border-gray-700">
+  <div id="logs" class="flex flex-col overflow-y-auto border-gray-700">
     {#each logs as log}
       <Log {log} />
     {/each}
