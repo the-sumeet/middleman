b7810edaa01d063dab548ab54ed772ba2b279b07
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-10-24 17:17:39 +0530

    autogen

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -75,11 +75,23 @@ export namespace main {
 	    }
 	}
 	export class HttpRequestLog {
+	    // Go type: time
+	    timestamp: any;
+	    scheme: string;
 	    method: string;
 	    host: string;
 	    path: string;
+	    requestHeaders: {[key: string]: string[]};
+	    responseHeaders: {[key: string]: string[]};
 	    requestBody: string;
 	    responseBody: string;
+	    cancelled: boolean;
+	    redirected: boolean;
+	    requestHeaderModified: boolean;
+	    responseHeaderModified: boolean;
+	    requestBodyModified: boolean;
+	    responseBodyModified: boolean;
+	    delayed: number;
 	
 	    static createFrom(source: any = {}) {
 	        return new HttpRequestLog(source);
@@ -87,12 +99,41 @@ export namespace main {
 	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.timestamp = this.convertValues(source["timestamp"], null);
+	        this.scheme = source["scheme"];
 	        this.method = source["method"];
 	        this.host = source["host"];
 	        this.path = source["path"];
+	        this.requestHeaders = source["requestHeaders"];
+	        this.responseHeaders = source["responseHeaders"];
 	        this.requestBody = source["requestBody"];
 	        this.responseBody = source["responseBody"];
+	        this.cancelled = source["cancelled"];
+	        this.redirected = source["redirected"];
+	        this.requestHeaderModified = source["requestHeaderModified"];
+	        this.responseHeaderModified = source["responseHeaderModified"];
+	        this.requestBodyModified = source["requestBodyModified"];
+	        this.responseBodyModified = source["responseBodyModified"];
+	        this.delayed = source["delayed"];
 	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
 	}
 	export class ModifyResponseBody {
 	    enabled: boolean;
