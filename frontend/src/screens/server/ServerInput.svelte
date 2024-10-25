26b7c20edd3d4e20b65173bf54646b92351d681c
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-10-25 23:49:22 +0530

    refactor server port inputs

---
+++ frontend/src/screens/server/ServerInput.svelte
@@ -0,0 +1,109 @@
+<script>
+    export let serverRunning;
+    export let port;
+    export let title;
+
+    import "bootstrap-icons/font/bootstrap-icons.css";
+    import { StartProxy, StopProxy, StartWebServer, StopWebServer } from "../../../wailsjs/go/main/App";
+    import { onDestroy, onMount } from "svelte";
+    import { GetConfig } from "../../../wailsjs/go/main/App";
+    import { GetWebServerPath } from "../../../wailsjs/go/main/App";
+    import { validatePort } from "./utils";
+
+    let isServerRunning;
+    const startButtonCss = "bg-blue-600 hover:bg-blue-500 focus:ring-blue-300";
+    const stopButtonCss = "bg-red-600 hover:bg-red-500 focus:ring-red-300";
+    let error = "";
+    let webServerPath;
+
+    GetWebServerPath().then((path) => {
+        webServerPath = path;
+    });
+
+    const unsubSubServerRunning = serverRunning.subscribe((value) => {
+        isServerRunning = value;
+    });
+
+    function toggleProxy() {
+        error = "";
+
+        if (isServerRunning === true) {
+            console.log("Stopping proxy");
+            StopProxy().then(() => {
+                serverRunning.set(false);
+                // serverRunning.set(false);
+                console.log("Proxy stopped");
+            });
+        } else {
+            const err = validatePort(port);
+            if (err != "") {
+                error = err;
+                return;
+            }
+            const portNumber = parseInt(port);
+            console.log("Starting proxy");
+            StartProxy(portNumber).then((result) => {
+                if (result.error != "") {
+                    error = result.error;
+                    serverRunning.set(false);
+                    return;
+                } else {
+                    serverRunning.set(true);
+                    console.log("Proxy started");
+                }
+            });
+        }
+    }
+
+    onDestroy(() => {
+        unsubSubServerRunning();
+    });
+</script>
+
+<h1 class="text-2xl font-medium text-gray-800 dark:text-white">{title}</h1>
+
+<div class="mt-10 flex gap-2 mx-auto items-center">
+    <input
+        readonly={isServerRunning}
+        bind:value={port}
+        type="text"
+        min="0"
+        placeholder="8080"
+        class="max-w-sm block placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
+    />
+    <button
+        on:click={toggleProxy}
+        class="{isServerRunning
+            ? stopButtonCss
+            : startButtonCss} flex items-center rounded-lg px-4 py-2.5 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform focus:outline-none focus:ring focus:ring-opacity-80"
+    >
+        {#if isServerRunning}
+            Stop Proxy Server
+        {:else}
+            Start Proxy Server
+        {/if}
+    </button>
+</div>
+
+
+
+{#if error != ""}
+    <div
+        class="mt-4 mx-auto flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800"
+    >
+        <div class="flex items-center justify-center w-12 bg-red-500">
+            <i class="text-white text-xl bi bi-exclamation-triangle"></i>
+        </div>
+
+        <div class="px-4 py-2 -mx-3">
+            <div class="mx-3">
+                <span class="font-semibold text-red-500 dark:text-red-400"
+                    >Error</span
+                >
+                <p class="text-sm text-gray-600 dark:text-gray-200">
+                    {error}
+                </p>
+            </div>
+        </div>
+    </div>
+{/if}
--- frontend/src/screens/server/ServerScreen.svelte
+++ frontend/src/screens/server/ServerScreen.svelte
@@ -1,119 +1,42 @@
 <script>
     import "bootstrap-icons/font/bootstrap-icons.css";
-    import { StartProxy, StopProxy } from "../../../wailsjs/go/main/App";
-    import { serverRunning } from "../../../src/stores";
-    import { onDestroy, onMount } from "svelte";
+    import { proxyServerRunning, webServerRunning } from "../../../src/stores";
+    import { onMount } from "svelte";
     import { GetConfig } from "../../../wailsjs/go/main/App";
+    import { GetWebServerPath } from "../../../wailsjs/go/main/App";
+    import ServerInput from "./ServerInput.svelte";
 
     let isServerRunning;
     const startButtonCss = "bg-blue-600 hover:bg-blue-500 focus:ring-blue-300";
     const stopButtonCss = "bg-red-600 hover:bg-red-500 focus:ring-red-300";
     let error = "";
     let port;
+    let proxyServerPort;
+    let webServerPort;
+    let webServerPath;
+
+    GetWebServerPath().then((path) => {
+        webServerPath = path;
+    });
 
     onMount(async () => {
         const config = await GetConfig();
-        port = config.serverPort;
-    });
-
-    const ubSubServerRunning = serverRunning.subscribe((value) => {
-        isServerRunning = value;
-    });
-
-    function validatePort() {
-        let portNumber = parseInt(port);
-        if (isNaN(portNumber)) {
-            error = "Invalid number";
-            return false;
-        }
-
-        if (portNumber.toString().length != 4) {
-            error = "Port must be of length 4";
-            return false;
-        }
-        return true;
-    }
-
-    function toggleProxy() {
-        error = "";
-
-        if (isServerRunning === true) {
-            console.log("Stopping proxy");
-            StopProxy().then(() => {
-                serverRunning.set(false);
-                // serverRunning.set(false);
-                console.log("Proxy stopped");
-            });
-        } else {
-            if (!validatePort()) {
-                return;
-            }
-            const portNumber = parseInt(port);
-            console.log("Starting proxy");
-            StartProxy(portNumber).then((result) => {
-                if (result.error != "") {
-                    error = result.error;
-                    serverRunning.set(false);
-
-                    return;
-                } else {
-                    serverRunning.set(true);
-
-                    console.log("Proxy started");
-                }
-            });
-        }
-    }
-
-    onDestroy(() => {
-        ubSubServerRunning();
+        proxyServerPort = config.proxyServerPort;
+        webServerPort = config.webServerPort;
     });
 </script>
 
 <div class="flex flex-col w-full p-4">
-    <h1 class="text-2xl font-medium text-gray-800 dark:text-white">Proxy Server</h1>
+    <ServerInput title={"Proxy Server"} serverRunning={proxyServerRunning} port={proxyServerPort} />
+    <ServerInput title={"Web Server"} serverRunning={webServerRunning} port={webServerPort} />
 
-    <div class="mt-10 flex gap-2 mx-auto items-center">
-        <input
-            readonly={isServerRunning}
-            bind:value={port}
-            type="text"
-            min="0"
-            placeholder="8080"
-            class="max-w-sm block placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
-        />
-        <button
-            on:click={toggleProxy}
-            class="{isServerRunning
-                ? stopButtonCss
-                : startButtonCss} flex items-center rounded-lg px-4 py-2.5 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform focus:outline-none focus:ring focus:ring-opacity-80"
-        >
-            {#if isServerRunning}
-                Stop Server
-            {:else}
-                Start Server
-            {/if}
-        </button>
-    </div>
-
-    {#if error != ""}
-        <div
-            class="mt-4 mx-auto flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800"
-        >
-            <div class="flex items-center justify-center w-12 bg-red-500">
-                <i class="text-white text-xl bi bi-exclamation-triangle"></i>
-            </div>
-
-            <div class="px-4 py-2 -mx-3">
-                <div class="mx-3">
-                    <span class="font-semibold text-red-500 dark:text-red-400"
-                        >Error</span
-                    >
-                    <p class="text-sm text-gray-600 dark:text-gray-200">
-                        {error}
-                    </p>
-                </div>
-            </div>
+    {#if webServerPath}
+        <div class="mx-auto mt-4 text-white">
+            <p>
+                Webserver running on <span class="p-2 text-blue-500 font-mono"
+                    >127.0.0.1:{port}{webServerPath}</span
+                >
+            </p>
         </div>
     {/if}
 </div>
