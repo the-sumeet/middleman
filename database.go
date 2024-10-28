db2c88b946d9ca55e75fe67a0e1f358b61c6e0ba
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-10-29 02:32:36 +0530

    moved rule type

--- database.go
+++ database.go
@@ -9,29 +9,6 @@ const (
 	MODIFY_RESPONSE_BODY = "modifyResponseBody"
 )
 
-type Header struct {
-	Action    string `json:"action"`
-	IsRequest bool   `json:"isRequest"`
-	Name      string `json:"name"`
-	Value     string `json:"value"`
-}
-
-type Rule struct {
-	Id                 int      `json:"id"`
-	Type               string   `json:"type"`
-	Enabled            bool     `json:"enabled"`
-	Entity             string   `json:"entity"`             // Like url, path, header etc
-	Op                 string   `json:"op"`                 // Like contains, equals, etc
-	Value              string   `json:"value"`              // The value to match
-	RedirectTo         string   `json:"redirectTo"`         // For redirect rule
-	DelaySec           int      `json:"delaySec"`           // For delay rule
-	RequestBody        string   `json:"requestBody"`        // For modify request body rule
-	ResponseBody       string   `json:"responseBody"`       // For modify response body rule
-	RequestHeaderMods  []Header `json:"requestHeaderMods"`  // For modify header rule
-	ResponseHeaderMods []Header `json:"responseHeaderMods"` // For modify header rule
-
-}
-
 type Database interface {
 	GetManyRules(recordType string) ([]Rule, error)
 	AddRule(rule Rule) (any, error)
--- rules.go
+++ rules.go
@@ -1,4 +1,24 @@
 package main
 
+type Header struct {
+	Action    string `json:"action"`
+	IsRequest bool   `json:"isRequest"`
+	Name      string `json:"name"`
+	Value     string `json:"value"`
+}
+
 type Rule struct {
+	Id                 int      `json:"id"`
+	Type               string   `json:"type"`
+	Enabled            bool     `json:"enabled"`
+	Entity             string   `json:"entity"`             // Like url, path, header etc
+	Op                 string   `json:"op"`                 // Like contains, equals, etc
+	Value              string   `json:"value"`              // The value to match
+	RedirectTo         string   `json:"redirectTo"`         // For redirect rule
+	DelaySec           int      `json:"delaySec"`           // For delay rule
+	RequestBody        string   `json:"requestBody"`        // For modify request body rule
+	ResponseBody       string   `json:"responseBody"`       // For modify response body rule
+	RequestHeaderMods  []Header `json:"requestHeaderMods"`  // For modify header rule
+	ResponseHeaderMods []Header `json:"responseHeaderMods"` // For modify header rule
+
 }
