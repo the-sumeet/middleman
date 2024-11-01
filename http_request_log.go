4610ac589d9e007611291cdd1f63c3d72fda2ce7
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-11-01 18:51:31 +0530

    updated HttpRequestLog with id field

--- http_request_log.go
+++ http_request_log.go
@@ -6,20 +6,22 @@ import (
 )
 
 type HttpRequestLog struct {
-	Timestamp              time.Time   `json:"timestamp"`
-	Scheme                 string      `json:"scheme"`
-	Method                 string      `json:"method"`
-	Host                   string      `json:"host"`
-	Path                   string      `json:"path"`
-	RequestHeaders         http.Header `json:"requestHeaders"`
-	ResponseHeaders        http.Header `json:"responseHeaders"`
-	RequestBody            string      `json:"requestBody"`
-	ResponseBody           string      `json:"responseBody"`
-	Cancelled              bool        `json:"cancelled"`
-	Redirected             bool        `json:"redirected"`
-	RequestHeaderModified  bool        `json:"requestHeaderModified"`
-	ResponseHeaderModified bool        `json:"responseHeaderModified"`
-	RequestBodyModified    bool        `json:"requestBodyModified"`
-	ResponseBodyModified   bool        `json:"responseBodyModified"`
-	Delayed                int         `json:"delayed"`
+	Id              string      `json:"id"`
+	Timestamp       time.Time   `json:"timestamp"`
+	Scheme          string      `json:"scheme"`
+	Method          string      `json:"method"`
+	Host            string      `json:"host"`
+	Path            string      `json:"path"`
+	RequestHeaders  http.Header `json:"requestHeaders"`
+	ResponseHeaders http.Header `json:"responseHeaders"`
+	RequestBody     string      `json:"requestBody"`
+	ResponseBody    string      `json:"responseBody"`
+	// Rules related information
+	Cancelled              bool `json:"cancelled"`
+	RedirectedTo           bool `json:"redirected"`
+	RequestHeaderModified  bool `json:"requestHeaderModified"`
+	ResponseHeaderModified bool `json:"responseHeaderModified"`
+	RequestBodyModified    bool `json:"requestBodyModified"`
+	ResponseBodyModified   bool `json:"responseBodyModified"`
+	Delayed                int  `json:"delayed"`
 }
