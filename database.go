1575b39bbb467a6d4e1e670dc9f82abdcdabda2c
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-11-01 18:50:55 +0530

    crud methods for http request logs

--- database.go
+++ database.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/http"
+
 const (
 	REDIRECT             = "redirect"
 	CANCEL               = "cancel"
@@ -15,4 +17,19 @@ type Database interface {
 	RemoveRule(id any) error
 	UpdateRule(id any, rule Rule) (Rule, error)
 	GetOneRule(id any) (Rule, error)
+	// Requests CRUD
+	AddRequest(request HttpRequestLog) (any, error)
+	UpdateRequest(id string, request HttpRequestLog) (HttpRequestLog, error)
+	GetOneRequest(id string) (HttpRequestLog, error)
+	GetManyRequests() ([]HttpRequestLog, error)
+	RemoveRequest(id any) error
+	SetRequestHeaders(id any, headers http.Header, isRequest bool) error
+	SetRequestBody(id any, body string, isRequest bool) error
+	SetRequestCancelled(id any, value bool) error
+	SetRequestDelayed(id any, value int) error
+	SetRequestRedirected(id any, value bool) error
+	SetRequestHeaderModified(id any, value bool) error
+	SetResponseHeaderModified(id any, value bool) error
+	SetRequestBodyModified(id any, value bool) error
+	SetResponseBodyModified(id any, value bool) error
 }
