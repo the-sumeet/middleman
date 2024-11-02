aa9df746395c7bd5d21db8a250ed38b718631925
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-11-02 14:46:50 +0530

    added getStatusColor and getMethodColor

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,10 +2,9 @@
 
 
 import { RemoveRule, GetManyRules, UpdateRule } from "../wailsjs/go/main/App";
-import {  errorMessage, modifyResponseBody, refreshList } from "./stores";
+import { errorMessage, modifyResponseBody, refreshList } from "./stores";
 import { RULE_DELAY, RULE_MODIFY_REQUEST_BODY, RULE_CANCEL, RULE_MOD_HEADER, RULE_REDIRECT, RULE_MODIFY_RESPONSE_BODY } from "./constants";
 import { main } from "../wailsjs/go/models";
-
 
 export const scrollToBottom = async (el) => {
   el.scroll({ top: el.scrollHeight, behavior: 'smooth' });
@@ -37,7 +36,7 @@ export const removeAndRefresh = async (type, id) => {
   if (error != "") {
     errorMessage.set(error);
     return;
-  }  
+  }
 }
 
 export const updateRule = async (id, rule) => {
@@ -60,3 +59,28 @@ export const updateAndRefresh = async (type, id, rule) => {
     return;
   }
 }
+
+export const getStatusColor = (status) => {
+  if (status >= 400) {
+    return "red";
+  } else if (status >= 300) {
+    return "gray";
+  } else if (status >= 200) {
+    return "yellow";
+  } else {
+    return "blue";
+  }
+}
+
+export const getMethodColor = (method) => {
+  if (method.toLowerCase() === "get") {
+    return "green";
+  } else if (method.toLowerCase() === "post" || method.toLowerCase() === "put" || method.toLowerCase() === "patch") {
+    return "blue";
+  } else if (method.toLowerCase() === "delete") {
+    return "red";
+  }
+
+  return "blue";
+}
+
