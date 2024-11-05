af48ef7e33469a5bb92d2bb472d4a1d33ff40b63
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-11-06 00:34:21 +0530

    saperate databases for rules and requests

--- database_sqlite.go
+++ database_sqlite.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/google/uuid"
 	_ "modernc.org/sqlite"
 )
 
@@ -23,14 +22,15 @@ const (
 )
 
 type SqliteDatabase struct {
-	filePath string
-	db       *sql.DB
+	filePath  string
+	ruleDb    *sql.DB
+	requestDb *sql.DB
 }
 
-func (s *SqliteDatabase) migrateVersion(from int) {
+func (s *SqliteDatabase) migrateVersion(from int, db *sql.DB) {
 	for i := from; i < len(UP); i++ {
 		ddl := UP[i]
-		_, err := s.db.Exec(ddl)
+		_, err := db.Exec(ddl)
 		if err != nil {
 			panic(err)
 		}
@@ -38,29 +38,60 @@ func (s *SqliteDatabase) migrateVersion(from int) {
 }
 
 func (s *SqliteDatabase) migrate() {
-	row := s.db.QueryRow(fmt.Sprintf("SELECT MAX(version) FROM %s", MIGRATION))
-
+	// Migrate rule database
+	row := s.ruleDb.QueryRow(fmt.Sprintf("SELECT MAX(version) FROM %s", MIGRATION))
 	if row.Err() != nil {
 		if strings.Contains(row.Err().Error(), ERR_NO_SUCH_TABLE) {
-			s.migrateVersion(0)
+			s.migrateVersion(0, s.ruleDb)
 		} else if strings.Contains(row.Err().Error(), ERR_FILE_NOT_DATABASE) {
 			// Panic
 		}
 	} else {
 		var version int
 		row.Scan(&version)
-		s.migrateVersion(version + 1)
+		s.migrateVersion(version+1, s.ruleDb)
+	}
+
+	// Migrate requests database
+	row = s.requestDb.QueryRow(fmt.Sprintf("SELECT MAX(version) FROM %s", MIGRATION))
+	if row.Err() != nil {
+		if strings.Contains(row.Err().Error(), ERR_NO_SUCH_TABLE) {
+			s.migrateVersion(0, s.requestDb)
+		} else if strings.Contains(row.Err().Error(), ERR_FILE_NOT_DATABASE) {
+			// Panic
+		}
+	} else {
+		var version int
+		row.Scan(&version)
+		s.migrateVersion(version+1, s.requestDb)
 	}
 }
 
-func NewSqliteDatabase(filePath string) *SqliteDatabase {
-	db, err := sql.Open("sqlite", filePath)
-	fmt.Println(err)
+func NewSqliteDatabase(ruleDbPath, requestDbPath string) *SqliteDatabase {
+	// Open databae, set journal mode to WAL, close and reopen.
+	ruleDb, err := sql.Open("sqlite", ruleDbPath)
 	if err != nil {
 		panic(err)
 	}
-	database := &SqliteDatabase{db: db, filePath: filePath}
+	_, err = ruleDb.Exec("PRAGMA journal_mode=WAL;")
+	ruleDb.Close()
+	ruleDb, err = sql.Open("sqlite", ruleDbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Open databae, set journal mode to WAL, close and reopen.
+	requestDb, err := sql.Open("sqlite", requestDbPath)
+	if err != nil {
+		panic(err)
+	}
+	_, err = requestDb.Exec("PRAGMA journal_mode=WAL;")
+	requestDb.Close()
+	requestDb, err = sql.Open("sqlite", requestDbPath)
+	requestDb.Exec("DELETE FROM request") // Clear table on start.
+	database := &SqliteDatabase{ruleDb: ruleDb, requestDb: requestDb, filePath: ruleDbPath}
 	database.migrate()
+
 	return database
 }
 
@@ -72,7 +103,7 @@ func (s *SqliteDatabase) AddRule(rule Rule) (any, error) {
 		return nil, err
 	}
 
-	res, err = s.db.Exec(fmt.Sprintf("INSERT INTO %s (data) VALUES (?)", RULE), marshalled)
+	res, err = s.ruleDb.Exec(fmt.Sprintf("INSERT INTO %s (data) VALUES (?)", RULE), marshalled)
 	if err != nil {
 		return nil, err
 	}
@@ -92,7 +123,7 @@ func (s *SqliteDatabase) UpdateRule(id any, rule Rule) (Rule, error) {
 		return rule, err
 	}
 
-	_, err = s.db.Exec(fmt.Sprintf("UPDATE %s SET data = ? WHERE id = ?", RULE), marshalled, id)
+	_, err = s.ruleDb.Exec(fmt.Sprintf("UPDATE %s SET data = ? WHERE id = ?", RULE), marshalled, id)
 	if err != nil {
 		return rule, err
 	}
@@ -110,7 +141,7 @@ func (s *SqliteDatabase) GetOneRule(id any) (Rule, error) {
 	var jsonData string
 	var rule Rule
 
-	row := s.db.QueryRow(fmt.Sprintf("SELECT id, data FROM %s WHERE id = ?", RULE), id)
+	row := s.ruleDb.QueryRow(fmt.Sprintf("SELECT id, data FROM %s WHERE id = ?", RULE), id)
 
 	err := row.Scan(&id, &jsonData)
 	if err != nil {
@@ -129,7 +160,7 @@ func (s *SqliteDatabase) GetOneRule(id any) (Rule, error) {
 func (s *SqliteDatabase) GetManyRules(recordType string) ([]Rule, error) {
 	rules := []Rule{}
 
-	rows, err := s.db.Query(fmt.Sprintf("SELECT id, data FROM %s WHERE json_extract(data, '$.type') = ?", RULE), recordType)
+	rows, err := s.ruleDb.Query(fmt.Sprintf("SELECT id, data FROM %s WHERE json_extract(data, '$.type') = ?", RULE), recordType)
 	if err != nil {
 		return nil, err
 	}
@@ -159,35 +190,33 @@ func (s *SqliteDatabase) GetManyRules(recordType string) ([]Rule, error) {
 }
 
 func (s *SqliteDatabase) RemoveRule(id any) error {
-	_, err := s.db.Exec(fmt.Sprintf("DELETE FROM %s WHERE id = ?", RULE), id)
+
+	_, err := s.ruleDb.Exec(fmt.Sprintf("DELETE FROM %s WHERE id = ?", RULE), id)
+
 	return err
 }
 
 // Requests CRUD
 
-func (s *SqliteDatabase) AddRequest(request HttpRequestLog) (any, error) {
-	var res sql.Result
-	requestId, err := uuid.NewUUID()
-	if err != nil {
-		return nil, err
-	}
+func (s *SqliteDatabase) AddRequest(requestId any, request *HttpRequestLog) (any, error) {
 
 	marshalled, err := json.Marshal(request)
 	if err != nil {
 		return nil, err
 	}
 
-	res, err = s.db.Exec(fmt.Sprintf("INSERT INTO %s (id, data) VALUES (?, ?)", REQUEST), requestId, marshalled)
+	_, err = s.requestDb.Exec(fmt.Sprintf("INSERT INTO %s (id, data) VALUES (?, ?)", REQUEST), requestId, marshalled)
 	if err != nil {
 		return nil, err
 	}
 
-	lastInsertId, err := res.LastInsertId()
-	if err != nil {
-		return nil, err
-	}
+	// Sqlite returning wrong last insert id, check this.
+	// lastInsertId, err := res.LastInsertId()
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	return lastInsertId, nil
+	return requestId, nil
 }
 
 func (s *SqliteDatabase) UpdateRequest(id string, request HttpRequestLog) (HttpRequestLog, error) {
@@ -197,7 +226,7 @@ func (s *SqliteDatabase) UpdateRequest(id string, request HttpRequestLog) (HttpR
 		return request, err
 	}
 
-	_, err = s.db.Exec(fmt.Sprintf("UPDATE %s SET data = ? WHERE id = ?", REDIRECT), marshalled, id)
+	_, err = s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = ? WHERE id = ?", REDIRECT), marshalled, id)
 	if err != nil {
 		return request, err
 	}
@@ -215,7 +244,7 @@ func (s *SqliteDatabase) GetOneRequest(id string) (HttpRequestLog, error) {
 	var jsonData string
 	var request HttpRequestLog
 
-	row := s.db.QueryRow(fmt.Sprintf("SELECT id, data FROM %s WHERE id = ?", REQUEST), id)
+	row := s.requestDb.QueryRow(fmt.Sprintf("SELECT id, data FROM %s WHERE id = ?", REQUEST), id)
 
 	err := row.Scan(&id, &jsonData)
 	if err != nil {
@@ -234,7 +263,8 @@ func (s *SqliteDatabase) GetOneRequest(id string) (HttpRequestLog, error) {
 func (s *SqliteDatabase) GetManyRequests() ([]HttpRequestLog, error) {
 	requests := []HttpRequestLog{}
 
-	rows, err := s.db.Query(fmt.Sprintf("SELECT id, data FROM %s", REQUEST))
+	// rows, err := s.requestDb.Query(fmt.Sprintf("SELECT id, data FROM %s", REQUEST))
+	rows, err := s.requestDb.Query(fmt.Sprintf("SELECT id, data FROM %s", REQUEST))
 	if err != nil {
 		return nil, err
 	}
@@ -264,11 +294,13 @@ func (s *SqliteDatabase) GetManyRequests() ([]HttpRequestLog, error) {
 }
 
 func (s *SqliteDatabase) RemoveRequest(id any) error {
-	_, err := s.db.Exec(fmt.Sprintf("DELETE FROM %s WHERE id = ?", REQUEST), id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("DELETE FROM %s WHERE id = ?", REQUEST), id)
+
 	return err
 }
 
-func (s *SqliteDatabase) SetRequestHeaders(id any, headers http.Header, isRequest bool) error {
+func (s *SqliteDatabase) SetHeaders(id any, headers http.Header, isRequest bool) error {
 	field := "requestHeaders"
 	if !isRequest {
 		field = "responseHeaders"
@@ -278,77 +310,97 @@ func (s *SqliteDatabase) SetRequestHeaders(id any, headers http.Header, isReques
 	if err != nil {
 		return err
 	}
-	_, err = s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.%s', ?) WHERE id = ?", REQUEST, field), marshalled, id)
+
+	_, err = s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.%s', ?) WHERE id = ?", REQUEST, field), marshalled, id)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
-func (s *SqliteDatabase) SetRequestBody(id any, body string, isRequest bool) error {
+func (s *SqliteDatabase) SetBody(id any, body string, isRequest bool) error {
 	field := "requestBody"
 	if !isRequest {
 		field = "responseBody"
 	}
-	_, err := s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.%s', ?) WHERE id = ?", REQUEST, field), body, id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.%s', ?) WHERE id = ?", REQUEST, field), body, id)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func (s *SqliteDatabase) SetRequestCancelled(id any, value bool) error {
-	_, err := s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.cancelled', ?) WHERE id = ?", REQUEST), value, id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.cancelled', ?) WHERE id = ?", REQUEST), value, id)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func (s *SqliteDatabase) SetRequestDelayed(id any, value int) error {
-	_, err := s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.delayed', ?) WHERE id = ?", REQUEST), value, id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.delayed', ?) WHERE id = ?", REQUEST), value, id)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func (s *SqliteDatabase) SetRequestRedirected(id any, value bool) error {
-	_, err := s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.redirected', ?) WHERE id = ?", REQUEST), value, id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.redirected', ?) WHERE id = ?", REQUEST), value, id)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func (s *SqliteDatabase) SetRequestHeaderModified(id any, value bool) error {
-	_, err := s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.requestHeaderModified', ?) WHERE id = ?", REQUEST), value, id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.requestHeaderModified', ?) WHERE id = ?", REQUEST), value, id)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func (s *SqliteDatabase) SetResponseHeaderModified(id any, value bool) error {
-	_, err := s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.responseHeaderModified', ?) WHERE id = ?", REQUEST), value, id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.responseHeaderModified', ?) WHERE id = ?", REQUEST), value, id)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func (s *SqliteDatabase) SetRequestBodyModified(id any, value bool) error {
-	_, err := s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.requestBodyModified', ?) WHERE id = ?", REQUEST), value, id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.requestBodyModified', ?) WHERE id = ?", REQUEST), value, id)
+
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func (s *SqliteDatabase) SetResponseBodyModified(id any, value bool) error {
-	_, err := s.db.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.responseBodyModified', ?) WHERE id = ?", REQUEST), value, id)
+
+	_, err := s.requestDb.Exec(fmt.Sprintf("UPDATE %s SET data = json_replace (data, '$.responseBodyModified', ?) WHERE id = ?", REQUEST), value, id)
+
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
