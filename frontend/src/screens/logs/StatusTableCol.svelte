891aac025bee25b50825a2052c585a26227c0705
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-11-08 00:39:45 +0530

    Widget for status in the log

---
+++ frontend/src/screens/logs/StatusTableCol.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let status;
+</script>
+
+
+{#if status >= 400}
+<td class="font-bold text-red-400 text-sm w-1/12 truncate border-r border-gray-700 px-1">{status}</td>
+{:else if status >= 300}
+<td class="font-bold text-yellow-400 text-sm w-1/12 truncate border-r border-gray-700 px-1">{status}</td>
+{:else if status >= 200}
+<td class="font-bold text-green-400 text-sm w-1/12 truncate border-r border-gray-700 px-1">{status}</td>
+{:else}
+<td class="font-bold text-gray-300 text-sm w-1/12 truncate border-r border-gray-700 px-1">{status}</td>
+{/if}
