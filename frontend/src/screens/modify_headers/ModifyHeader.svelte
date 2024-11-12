1e1924df5daa628d7b2c8a02dc2a4fcf7c87b176
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-11-13 05:02:15 +0530

    refactor for saperate mod headres

--- frontend/src/screens/modify_headers/ModifyHeader.svelte
+++ frontend/src/screens/modify_headers/ModifyHeader.svelte
@@ -1,20 +1,15 @@
 <script>
     export let rule;
+    export let headerType;
 
     import { main } from "../../../wailsjs/go/models";
-    import { UpdateRule } from "../../../wailsjs/go/main/App";
     import { errorMessage } from "../../stores";
-    import { GetManyRules } from "../../../wailsjs/go/main/App";
     import BottomButtons from "../../../src/widgets/BottomButtons.svelte";
     import EntitySelect from "../../../src/widgets/EntitySelect.svelte";
     import { removeAndRefresh, updateRule } from "../../../src/utils";
-    import {
-        RULE_MOD_HEADER,
-        tabSelectedStyle,
-        tabUnselectedStyle,
-    } from "../../../src/constants";
+    import { RULE_MOD_REQUEST_HEADER } from "../../../src/constants";
+    import RuleTile from "../rules/RuleTile.svelte";
 
-    let requestTab = true;
     let changed = false;
     let entity;
     let op;
@@ -34,77 +29,52 @@
 
     function addMod() {
         const newMod = new main.Header({
-            isRequest: requestTab,
-            action: "remove",
-            name: "name",
-            value: "value",
+            action: "add",
+            name: "Foo",
+            value: "Bar",
         });
-        if (requestTab == true) {
-            const currentRequestMods =
-                rule.requestHeaderMods != undefined ||
-                rule.requestHeaderMods != null
-                    ? rule.requestHeaderMods
-                    : [];
-            rule.requestHeaderMods = [...currentRequestMods, newMod];
-        } else {
-            const currentResponseMods =
-                rule.responseHeaderMods != undefined ||
-                rule.responseHeaderMods != null
-                    ? rule.responseHeaderMods
-                    : [];
-            rule.responseHeaderMods = [...currentResponseMods, newMod];
-        }
+        const currentRequestMods =
+            rule.headerMods != undefined || rule.headerMods != null
+                ? rule.headerMods
+                : [];
+        rule.headerMods = [...currentRequestMods, newMod];
+
         changed = true;
     }
 
     function removeMod(index) {
-        if (requestTab) {
-            rule.requestHeaderMods = rule.requestHeaderMods.filter((_, i) => i !== index);
-        } else {
-            rule.responseHeaderMods = rule.responseHeaderMods.filter((_, i) => i !== index);
-        }
-        
+        rule.headerMods = rule.headerMods.filter((_, i) => i !== index);
+
         changed = true;
     }
 
-    function setAction(index, action) {        
-        if (requestTab) {
-            rule.requestHeaderMods[index].action = action;
-        } else {
-            rule.responseHeaderMods[index].action = action;
-        }
-        changed = true;
-    }
+    // function setAction(index, action) {
+    //     if (headerType == RULE_MOD_REQUEST_HEADER) {
+    //         rule.requestHeaderMods[index].action = action;
+    //     } else {
+    //         rule.responseHeaderMods[index].action = action;
+    //     }
+    //     changed = true;
+    // }
 
     function setName(e, i) {
-        const name = e.target.value;
-        if (requestTab) {
-            rule.requestHeaderMods[i].name = name;
-        } else {
-            rule.responseHeaderMods[i].name = name;
-        }
+        rule.headerMods[i].name = e.target.value;
         changed = true;
     }
 
     function setValue(e, i) {
-        const value = e.target.value;
-        if (requestTab) {
-            rule.requestHeaderMods[i].value = value;
-        } else {
-            rule.responseHeaderMods[i].value = value;
-        }
+        rule.headerMods[i].value = e.target.value;
         changed = true;
     }
 
     function save() {
         const modifyHeaderRecord = new main.Rule({
-            type: RULE_MOD_HEADER,
+            type: headerType,
             enabled: rule.enabled,
             entity: entity,
             op: op,
             value: value,
-            requestHeaderMods: rule.requestHeaderMods,
-            responseHeaderMods: rule.responseHeaderMods,
+            headerMods: rule.headerMods,
         });
 
         updateRule(rule.id, modifyHeaderRecord).then(async (result) => {
@@ -129,17 +99,17 @@
     }
 </script>
 
-<div class="p-2 flex flex-col rounded-md bg-gray-800 border border-gray-700">
-    <h1 class="text-md text-white">If</h1>
+<div class="rule">
+    <h1 class="font-bold text-sm text-white">If</h1>
 
-    <div class="flex items-center justify-center gap-2 p-4 rounded-md mt-4">
+    <div class="mx-2 flex items-center justify-center gap-2 rounded-md mt-2">
         <EntitySelect bind:entity {setChanged} />
 
         <!-- Op -->
         <select
             on:change={setChanged}
             bind:value={op}
-            class="p-2 rounded text-whites"
+            class=""
             name=""
             id=""
         >
@@ -154,42 +124,18 @@
             autocorrect="off"
             type="text"
             placeholder="example"
-            class="w-full placeholder-gray-500 rounded-lg border px-5 py-2.5 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 border-gray-600 bg-gray-900 text-gray-300 focus:border-blue-300"
+            class="w-full placeholder-gray-500 rounded-lg border px-5 py-2 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 border-gray-600 bg-gray-900 text-gray-300 focus:border-blue-300"
         />
     </div>
 
-    <h1 class="mt-4 text-md text-white">Then Modify the Headers</h1>
+    <h1 class="mt-2 font-bold text-sm text-white">Then Modify the Headers</h1>
 
-    <div
-        class="mt-4 flex overflow-x-auto overflow-y-hidden border-b whitespace-nowrap border-gray-700"
-    >
-        <button
-            on:click={() => (requestTab = true)}
-            class="inline-flex whitespace-nowrap items-center h-10 px-2 py-2 -mb-px text-center bg-transparent border-b-2 sm:px-4 -px-1 {requestTab ===
-            true
-                ? tabSelectedStyle
-                : tabUnselectedStyle}"
-        >
-            <span class="mx-1 text-sm sm:text-base"> Request Headers </span>
-        </button>
-
-        <button
-            on:click={() => (requestTab = false)}
-            class="inline-flex whitespace-nowrap items-center h-10 px-2 py-2 -mb-px text-center bg-transparent border-b-2 sm:px-4 -px-1 {requestTab ===
-            false
-                ? tabSelectedStyle
-                : tabUnselectedStyle}"
-        >
-            <span class="mx-1 text-sm sm:text-base"> Response Headers </span>
-        </button>
-    </div>
-
-    <div class="mt-4 border-2 rounded-md border-gray-700">
-        {#if (requestTab && rule.requestHeaderMods) || (!requestTab && rule.responseHeaderMods)}
-            {#each requestTab ? rule.requestHeaderMods : rule.responseHeaderMods as mod, i}
+    <div class="mx-2 mt-2 border-2 rounded-md border-gray-700">
+        {#if rule.headerMods}
+            {#each rule.headerMods as mod, i}
                 <div class="flex space-x-2 p-4">
                     <select
-                        on:change={() => changed = true}
+                        on:change={() => (changed = true)}
                         bind:value={mod.action}
                         class="p-2 rounded text-whites"
                         name=""
@@ -245,7 +191,7 @@
         {changed}
         {save}
         {cancelSave}
-        remove={() => removeAndRefresh(RULE_MOD_HEADER, rule.id)}
+        remove={() => removeAndRefresh(headerType, rule.id)}
         {enableDisable}
         enabled={rule.enabled}
     />
