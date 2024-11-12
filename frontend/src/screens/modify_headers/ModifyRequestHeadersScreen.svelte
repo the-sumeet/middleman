7a01ac97b0ef02722924da36774c90f223f97039
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-11-13 05:01:58 +0530

    refactor for saperate mod headres

---
+++ frontend/src/screens/modify_headers/ModifyRequestHeadersScreen.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from "svelte";
+    import { modifyRequestHeaders } from "../../stores";
+    import ModifyHeader from "./ModifyHeader.svelte";
+    import { refreshList } from "../../stores";
+    import { RULE_MOD_REQUEST_HEADER } from "../../constants";
+
+    onMount(() => {
+        console.debug(`Updating ${RULE_MOD_REQUEST_HEADER} on mount`);
+        refreshList(RULE_MOD_REQUEST_HEADER);
+    });
+</script>
+
+{#if $modifyRequestHeaders.length > 0}
+    {#each $modifyRequestHeaders as modifyHeader, i (crypto.randomUUID())}
+        <div class="mb-4">
+            <ModifyHeader
+                headerType={RULE_MOD_REQUEST_HEADER}
+                rule={modifyHeader}
+            />
+        </div>
+    {/each}
+{/if}
---
+++ frontend/src/screens/modify_headers/ModifyResponseHeadersScreen.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from "svelte";
+    import { modifyResponseHeaders } from "../../stores";
+    import ModifyHeader from "./ModifyHeader.svelte";
+    import { refreshList } from "../../stores";
+    import { RULE_MOD_RESPONSE_HEADER } from "../../constants";
+
+    onMount(() => {
+        console.debug(`Updating ${RULE_MOD_RESPONSE_HEADER} on mount`);
+        refreshList(RULE_MOD_RESPONSE_HEADER);
+    });
+    
+</script>
+
+{#if $modifyResponseHeaders.length > 0}
+    {#each $modifyResponseHeaders as modifyHeader, i (crypto.randomUUID())}
+    <div class="mb-4">
+        <ModifyHeader headerType={RULE_MOD_RESPONSE_HEADER} rule={modifyHeader} />
+    </div>
+    {/each}
+{/if}
