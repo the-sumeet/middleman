409a19781006cf8286fe264fc1370bb5b8d30fa3
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-11-14 02:03:27 +0530

    update ciphersuit

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"fmt"
 	"io"
 	"log"
@@ -39,7 +40,20 @@ func NewApp() *App {
 	}
 
 	proxy := goproxy.NewProxyHttpServer()
-
+	proxy.Tr.TLSClientConfig = &tls.Config{
+		InsecureSkipVerify: true,
+		CipherSuites: []uint16{
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		},
+	}
 	app := &App{
 		proxy:          proxy,
 		database:       NewSqliteDatabase(config.RuleDbPath, config.RequestDbPath),
