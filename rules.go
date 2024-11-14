ad695cc65160cf4ee4212da1334b01746c3d3d2b
sumeetmathpati <sumeet.mathpati@gmail.com>
2024-11-14 07:49:28 +0530

    fmt

--- rules.go
+++ rules.go
@@ -8,18 +8,16 @@ type Header struct {
 }
 
 type Rule struct {
-	Id                 any      `json:"id"`
-	Type               string   `json:"type"`
-	Enabled            bool     `json:"enabled"`
-	Entity             string   `json:"entity"`             // Like url, path, header etc
-	Op                 string   `json:"op"`                 // Like contains, equals, etc
-	Value              string   `json:"value"`              // The value to match
-	RedirectTo         string   `json:"redirectTo"`         // For redirect rule
-	DelaySec           int      `json:"delaySec"`           // For delay rule
-	RequestBody        string   `json:"requestBody"`        // For modify request body rule
-	ResponseBody       string   `json:"responseBody"`       // For modify response body rule
-	RequestHeaderMods  []Header `json:"requestHeaderMods"`  // For modify header rule
-	ResponseHeaderMods []Header `json:"responseHeaderMods"` // For modify header rule
-	HeaderMods         []Header `json:"headerMods"`         // For modify header rule
+	Id           any      `json:"id"`
+	Type         string   `json:"type"`
+	Enabled      bool     `json:"enabled"`
+	Entity       string   `json:"entity"`       // Like url, path, header etc
+	Op           string   `json:"op"`           // Like contains, equals, etc
+	Value        string   `json:"value"`        // The value to match
+	RedirectTo   string   `json:"redirectTo"`   // For redirect rule
+	DelaySec     int      `json:"delaySec"`     // For delay rule
+	RequestBody  string   `json:"requestBody"`  // For modify request body rule
+	ResponseBody string   `json:"responseBody"` // For modify response body rule
+	HeaderMods   []Header `json:"headerMods"`   // For modify header rule
 
 }
